93f9165153851ec96e35831b070f76d6919741da
Hanatte <noreply@github.com>
2025-01-24 18:48:16 +0900

    Change fabric-api dependency from modCompileOnly to modImplementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ val polymerVersion: String by project
 dependencies {
     minecraft("com.mojang:minecraft:$minecraftVersion")
     mappings("net.fabricmc:yarn:$yarnVersion:v2")
-    modCompileOnly("net.fabricmc.fabric-api:fabric-api:$fabricApiVersion")
     modImplementation("net.fabricmc:fabric-loader:$fabricLoaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricApiVersion")
     modImplementation("eu.pb4:polymer-virtual-entity:$polymerVersion")
 }
 
